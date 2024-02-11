14d7e7959482f1429ec7c2ffa64d6037dcc15062
Paulo Lucas <paulolucasjs@gmail.com>
2024-02-11 12:44:53 -0300

    final shape (agora sim)

--- Day-1/day-1.py
+++ Day-1/day-1.py
@@ -4,7 +4,8 @@ def part1(string):
         if string[i].isdigit():
             digits.append(string[i])
     return int(digits[0] + digits[-1])
-    
+
+
 def part2(string):
     my_dict = {"one": "1", "two": "2", "three": "3", "four": "4",
                "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
@@ -18,8 +19,8 @@ def part2(string):
                     digits.append(my_dict[key])
     return int(digits[0] + digits[-1])
 
+
 values_sum = 0
-count = 1
 with open('./text.txt') as file:
     for line in file:
         values_sum += part1(line)
